4ba40d64d53aeb6c33853091dfd3ea82a78c13c9
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2023-10-14 23:57:47 +0400

    ai day7

---
+++ Day 7/classwotk.py
@@ -0,0 +1,22 @@
+#i=10
+#I=i+1
+#print(i)
+
+#numewo = 0
+
+#while numewo < 20:
+    #numewo += 4
+    #print(numewo)
+
+
+    #shevqmenit saitvacio cikli
+    #numewo = 0
+    #shevqmenit limiti 20mde
+    #tito iteaciaze pwintawt saitevawio cvlad numewos
+    #tito iteaaciaze vadidebt 4it numewo
+
+#i = 1
+#while i <= 100:
+    #print(i)
+    #i = i+1
+#print(i+i*(50-1))
---
+++ Day 7/homewotk.py
@@ -0,0 +1,7 @@
+tickets=13
+adult=10
+kids=0
+
+tickets=13+12
+
+print(int(input(tickets*adult+kids)))
